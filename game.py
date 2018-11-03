bdcdc0a7f417431922e9fe4671b5814dfe0f4c97
shivangisrivastava0408 <shivangidhn1998@gmail.com>
2018-11-03 21:25:10 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,72 @@
+import random
+print "                                                                  STONE , PAPER , SCISSORS"
+print "1.Enter 'st' for stone."
+print "2.Enter 'p' for paper."
+print "3.Enter 'sc' for scissors."
+c1 = 0 # player's points
+c2 = 0 # computer's points
+list = ['st','p','sc']
+a = 5
+while (a):
+        a = a-1
+        s1 = raw_input("Enter your move : ")
+        print "Computer, your turn!!"
+        s2 = list[random.randint(0,len(list)-1)]
+        print s2
+        if s1 == s2:
+                print "Same move"
+        elif s1 == 'st' and s2 == 'p':
+                c2 += 1
+                print "Computer, good job!"
+                print "Player :",c1
+                print "Computer :",c2
+        elif s1 == 'st' and s2 == 'sc':
+                c1 += 1
+                print "Player, good job!"
+                print "Player :",c1
+                print "Computer :",c2
+        elif s1 == 'p' and s2 == 'sc':
+                c2 += 1
+                print "Computer, good job!"
+                print "Player :",c1
+                print "Computer :",c2
+        elif s1 == 'p' and s2 == 'st':
+                c1 += 1
+                print "Player, good job!"
+                print "Player :",c1
+                print "Computer :",c2
+        elif s1 == 'sc' and s2 == 'st':
+                c2 += 1
+                print "Computer, good job!"
+                print "Player :",c1
+                print "Computer :",c2
+        elif s1 == 'sc' and s2 == 'p':
+                c1 += 1
+                print "Player, good job!"
+                print "Player :",c1
+                print "Computer :",c2
+if c1 > c2:
+        print "Player, you won!!!!!"
+elif c2 > c1:
+        print "Computer, you won!!!!!"
+else:
+        print "It's a draw."
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
